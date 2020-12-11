d8f74e19641c47210dca4ae5b1ee35c0d586822c
Batstone <adam.batstone@gmail.com>
2020-12-10 18:37:18 -0600

    mobile nav now working. need to add sticky nav

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    $('.header__mobile-icon-container').on('click', () => {
+        $('.js-menu')
+            .toggleClass('header__mobile-icon')
+            .toggleClass('header__mobile-icon--active')
+
+        if ($('.header__nav__list').css('display') === 'none') {
+            $('.header__nav__list').css('display', 'flex')
+        } else {
+            $('.header__nav__list').css('display', 'none')
+        };
+    })
+
+
+
+
+});
+
+
