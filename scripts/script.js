2784d475e649f430fb48e66b6a37b9b91e8e09ab
Batstone <adam.batstone@gmail.com>
2020-12-14 16:51:21 -0600

    added a before element in the header

--- scripts/script.js
+++ scripts/script.js
@@ -9,23 +9,27 @@ $(function () {
             $('.header__nav__list').css('display', 'flex')
         } else {
             $('.header__nav__list').css('display', 'none')
+            $('.header').css('padding-bottom', '10px')
         };
+
+        if ($('.js-menu').hasClass('header__mobile-icon--active') && $('.header').hasClass('sticky')) {
+            $('.header').css('padding-bottom', '150px')
+        }
     })
 
-    $('.js--main-intro').waypoint(function (direction) {
+    $('.js--main-nav').waypoint(function (direction) {
         if (direction === "down") {
             $('.header')
                 .addClass('sticky')
+                .removeClass('element')
         } else {
             $('.header')
                 .removeClass('sticky')
+                .addClass('element')
         }
     }, {
         offset: '40px;'
     });
-
-
-
 
 });
 
