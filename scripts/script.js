1f15ec76d5b650e00d51488380822193d750ab1a
Batstone <adam.batstone@gmail.com>
2020-12-16 17:03:31 -0600

    fixing overflow on header and footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 $(function () {
 
+    AOS.init();
+
     $('.header__mobile-icon-container').on('click', () => {
         $('.js-menu')
             .toggleClass('header__mobile-icon')
@@ -9,12 +11,12 @@ $(function () {
             $('.header__nav__list').css('display', 'flex')
         } else {
             $('.header__nav__list').css('display', 'none')
-            $('.header').css('padding-bottom', '10px')
+            // $('.header').css('padding-bottom', '10px')
         };
 
-        if ($('.js-menu').hasClass('header__mobile-icon--active') && $('.header').hasClass('sticky')) {
-            $('.header').css('padding-bottom', '150px')
-        }
+        // if ($('.js-menu').hasClass('header__mobile-icon--active') && $('.header').hasClass('sticky')) {
+        //     $('.header').css('padding-bottom', '150px')
+        // }
     })
 
     $('.js--main-nav').waypoint(function (direction) {
@@ -23,12 +25,14 @@ $(function () {
                 .addClass('sticky')
             $('.header__img').css('display', 'none')
             $('.header__nav__list').css('justify-content', 'center')
+            $('.header__nav__list__item').css('background-color', 'rgba(255, 255, 255, 0.0)')
         } else {
             $('.header')
                 .removeClass('sticky')
             $('.header__img').css('display', 'block')
-            $('.header__nav__list').css('justify-content', 'flex-start')
-            $('.header__nav__list').css('justify-content', 'center')
+            $('.header__nav__list').css('justify-content', 'flex-end')
+            $('.header__nav__list__item').css('background-color', 'black')
+
         }
     }, {
         offset: '40px;'
